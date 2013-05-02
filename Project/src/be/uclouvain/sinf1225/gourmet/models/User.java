db581733239346c5bcf6101678150c82951c9690
Guillaume Derval <guillaume@itself.be>
2013-05-02 16:24:22 +0200

    Login form (not working yet) + update doc

--- Project/src/be/uclouvain/sinf1225/gourmet/models/User.java
+++ Project/src/be/uclouvain/sinf1225/gourmet/models/User.java
@@ -2,6 +2,42 @@ package be.uclouvain.sinf1225.gourmet.models;
 
 public class User
 {
-	//TODO implement.
+	public enum UserManagerReturn
+	{
+		LOGIN_OK,
+		LOGIN_ERR,
+		REGISTER_MAIL_ALREADY_TAKEN,
+		NOT_FULL
+	}
+	
+	private static User loggedUser = null;
+	
+	public static boolean isUserConnected()
+	{
+		return loggedUser != null;
+	}
+	
+	public static User getUserConnected()
+	{
+		return loggedUser;
+	}
+	
+	public static UserManagerReturn loginUser(String email, String password)
+	{
+		//TODO implement.
+		return UserManagerReturn.LOGIN_ERR;
+	}
+	
+	public static UserManagerReturn registerUser(String name, String surname, String email, String password)
+	{
+		//TODO implement.
+		return UserManagerReturn.REGISTER_MAIL_ALREADY_TAKEN;
+	}
+	
+	public static void logoutUser()
+	{
+		loggedUser = null;
+	}
+	
 	public String getName() { return null; } //TODO implement.
 }
