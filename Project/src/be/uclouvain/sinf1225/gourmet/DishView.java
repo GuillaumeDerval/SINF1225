51cddbe20b13f87b7fc796cfa55f040ebcdad0e9
oliviab <olivia060@hotmail.com>
2013-05-05 11:35:44 +0200

    added image, description, price and checkboxes in DishView

--- Project/src/be/uclouvain/sinf1225/gourmet/DishView.java
+++ Project/src/be/uclouvain/sinf1225/gourmet/DishView.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import be.uclouvain.sinf1225.gourmet.models.Dish;
 import be.uclouvain.sinf1225.gourmet.utils.GourmetUtils;
@@ -38,10 +39,30 @@ public class DishView extends Activity
 		dish = Dish.getDish(getIntent().getExtras().getInt("dishId"));
 
 		((TextView) findViewById(R.id.DishViewName)).setText(dish.getName());
+		((TextView) findViewById(R.id.DishViewDescription)).setText(dish.getDescription());
+		((TextView) findViewById(R.id.DishViewPrice)).setText("" + dish.getPrice());
 		
 		// ((TextView)findViewById(R.id.RestaurantListDistance)).setText(new
 		// DecimalFormat("#.##").format(city.getLocation().distanceTo(locationListener.getLastLocation())/1000));
 		
+		final CheckBox ViewSpicy = (CheckBox) findViewById(R.id.DishViewSpicy);
+		final CheckBox ViewVegan = (CheckBox) findViewById(R.id.DishViewVegan);
+		final CheckBox ViewAvailable = (CheckBox) findViewById(R.id.DishViewAvailable);
+		
+		if (dish.getSpicy() == 1)
+			ViewSpicy.setChecked(true); // to be checked, true equals 1 or 0
+		else
+			ViewSpicy.setChecked(false);
+
+		if (dish.getVegan() == 1)
+			ViewVegan.setChecked(true);
+		else
+			ViewVegan.setChecked(false);
+
+		if (dish.getAllergen() == 1)
+			ViewAvailable.setChecked(true);
+		else
+			ViewAvailable.setChecked(false);
 
 	}
 }
