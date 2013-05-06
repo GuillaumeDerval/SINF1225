e4f60998e6fec07c68b24548ce38f84be84dd54c
Adrien Horgnies <adrien.horgnies@student.uclouvain.be>
2013-05-06 14:52:22 +0200

    rsv_mgr starting point of loading method :x

--- Project/src/be/uclouvain/sinf1225/gourmet/ReservationManagerView.java
+++ Project/src/be/uclouvain/sinf1225/gourmet/ReservationManagerView.java
@@ -2,7 +2,9 @@ package be.uclouvain.sinf1225.gourmet;
 
 import be.uclouvain.sinf1225.gourmet.utils.GourmetUtils;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup.LayoutParams;
@@ -23,43 +25,47 @@ public class ReservationManagerView extends Activity
 		return GourmetUtils.onMenuItemSelected(item, this);
 	}
 	
-	@SuppressWarnings("deprecation")
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		TableLayout rsv_tab = (TableLayout)findViewById(R.id.rsv_manager);
-		/*
+		setContentView(R.layout.activity_reservation_manager);
 		
-		TableRow rsv_item = new TableRow(this);
+		TableLayout rsv_tab = (TableLayout)findViewById(R.id.rsv_manager);
+		
+		TableRow row = (TableRow) ( (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.reservation_manager_row, null);
+		rsv_tab.addView(row);
+		((TextView)row.findViewById(R.id.reservationRowName)).setText("Du texte");
+		
+		/*TableRow rsv_item = new TableRow(this);
         rsv_item.setLayoutParams(new LayoutParams(
                 LayoutParams.FILL_PARENT,
                 LayoutParams.WRAP_CONTENT));
         
         TextView name = new TextView(this);
         name.setText("Horgnies");
-        name.setId(5);
+        name.setId(1);
         name.setLayoutParams(new LayoutParams(
                 LayoutParams.FILL_PARENT,
                 LayoutParams.WRAP_CONTENT));
         
         TextView date = new TextView(this);
         date.setText("06-05-13");
-        date.setId(5);
+        date.setId(2);
         date.setLayoutParams(new LayoutParams(
                 LayoutParams.FILL_PARENT,
                 LayoutParams.WRAP_CONTENT));
         
         TextView time = new TextView(this);
         time.setText("18:30");
-        time.setId(5);
+        time.setId(3);
         time.setLayoutParams(new LayoutParams(
                 LayoutParams.FILL_PARENT,
                 LayoutParams.WRAP_CONTENT));
         
         TextView restaurant = new TextView(this);
         restaurant.setText("Le sot l'y laisse");
-        restaurant.setId(5);
+        restaurant.setId(4);
         restaurant.setLayoutParams(new LayoutParams(
                 LayoutParams.FILL_PARENT,
                 LayoutParams.WRAP_CONTENT));
@@ -71,18 +77,17 @@ public class ReservationManagerView extends Activity
                 LayoutParams.FILL_PARENT,
                 LayoutParams.WRAP_CONTENT));
         
-        rsv_item.addView(name);
-        rsv_item.addView(date);
-        rsv_item.addView(time);
-        rsv_item.addView(restaurant);
-        rsv_item.addView(number);
+        ((TableRow) rsv_item).addView(name);
+        ((TableRow) rsv_item).addView(date);
+        ((TableRow) rsv_item).addView(time);
+        ((TableRow) rsv_item).addView(restaurant);
+        ((TableRow) rsv_item).addView(number);
         
 
-        rsv_tab.addView(rsv_item,new TableLayout.LayoutParams(
+        ((TableLayout) rsv_tab).addView(rsv_item,new TableLayout.LayoutParams(
                   LayoutParams.MATCH_PARENT,
                   LayoutParams.WRAP_CONTENT));*/
         
-
-		setContentView(R.layout.activity_reservation_manager);
+        System.out.println("COUCOU !");
 	}
 }
