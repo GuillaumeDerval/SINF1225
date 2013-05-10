10134bfcc5929f1d135c65ff5ee433d88797e3f6
Adrien Horgnies <adrien.horgnies@student.uclouvain.be>
2013-05-10 19:15:14 +0200

    pref mgr SaveButton now show state of checkBox

--- Project/src/be/uclouvain/sinf1225/gourmet/PreferenceManagerView.java
+++ Project/src/be/uclouvain/sinf1225/gourmet/PreferenceManagerView.java
@@ -2,11 +2,16 @@ package be.uclouvain.sinf1225.gourmet;
 
 import be.uclouvain.sinf1225.gourmet.utils.GourmetUtils;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class PreferenceManagerView extends Activity
 {
@@ -32,6 +37,23 @@ public class PreferenceManagerView extends Activity
 		ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.pref_budget_list, android.R.layout.simple_spinner_item);
 		adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		budgetSpinner.setAdapter(adapter1);
+		
+		final Button saveButton = (Button) findViewById(R.id.saveButton);
+		saveButton.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v) {
+				
+				final CheckBox allergenBox = (CheckBox) findViewById(R.id.allergen_checkbox);
+				final CheckBox vegBox = (CheckBox) findViewById(R.id.veg_checkbox);
+				final CheckBox spicyBox = (CheckBox) findViewById(R.id.spicy_checkbox);
+				boolean allergenBool = allergenBox.isChecked();
+				boolean vegBool = vegBox.isChecked();
+				boolean spicyBool = spicyBox.isChecked();
+				
+				Context context = getApplicationContext();
+				Toast toast = Toast.makeText(context, "allergen = "+allergenBool+"\nvegetarien = "+vegBool+"\nspicy = "+spicyBool, Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		});
 		
 	}
 }
